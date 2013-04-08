281ea55daad10d06d5ec0f0ea81149e03e99fd22
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-08 21:01:07 +0200

    namepace actions (app / server)

--- lib/chaos/cli.rb
+++ lib/chaos/cli.rb
@@ -4,34 +4,42 @@ module Chaos
 
   class CLI < Thor
 
-    desc "bootstrap", "Bootstrap a server"
-    method_option :ssh, aliases: "-s", desc: 'ssh url used to connect to the server', required: true
-    def bootstrap
-      user = ENV['USER']
-      server = Chaos::Server.new options[:ssh]
-      server.bootstrap
-      server.run_chef true
-      server.register_git_user user
-      display "Done.", :topic
-      display "Default passwords set for admin users by chef is their user names."
-      display "Connect to the server to change it ('ssh #{user}@#{server.host}') before doing anything else."
+    class Server < Thor
+      namespace :server
+
+      desc "server:bootstrap", "Bootstrap a server"
+      method_option :ssh, aliases: "-s", desc: 'ssh url used to connect to the server', required: true
+      def bootstrap
+        user = ENV['USER']
+        server = Chaos::Server.new options[:ssh]
+        server.bootstrap
+        server.run_chef true
+        server.register_git_user user
+        display "Done.", :topic
+        display "Default passwords set for admin users by chef is their user names."
+        display "Connect to the server to change it ('ssh #{user}@#{server.host}') before doing anything else."
+      end
+
+      desc "server:update", "Update a server configuration running chef"
+      method_option :server, aliases: "-s", desc: 'server on which the app will be published (host[:port])', required: true
+      def update
+        server = Chaos::Server.new "ssh://#{options[:server]}"
+        server.run_chef
+      end
     end
 
-    desc "update", "Update a server configuration running chef"
-    method_option :server, aliases: "-s", desc: 'server on which the app will be published (host[:port])', required: true
-    def update
-      server = Chaos::Server.new "ssh://#{options[:server]}"
-      server.run_chef
-    end
+    class App < Thor
+      namespace :app
 
-    desc "create", "Create an application on the server"
-    method_option :server, aliases: "-s", desc: 'server on which the app will be published (host[:port])', required: true
-    method_option :name, aliases: "-n", desc: 'name of the app'
-    def create
-      server = Chaos::Server.new "ssh://#{options[:server]}"
-      name = options[:name] || File.basename(Dir.pwd)
-      app = Chaos::App.new name
-      app.create server
+      desc "app:create", "Create an application on the server"
+      method_option :server, aliases: "-s", desc: 'server on which the app will be published (host[:port])', required: true
+      method_option :name, aliases: "-n", desc: 'name of the app'
+      def create
+        server = Chaos::Server.new "ssh://#{options[:server]}"
+        name = options[:name] || File.basename(Dir.pwd)
+        app = Chaos::App.new name
+        app.create server
+      end
     end
   end
 end
